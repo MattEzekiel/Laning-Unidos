46a793a4b0c8e1cee3c68a2a937f8443b290d659
Matias Bubello <matt.bubello@gmail.com>
2022-04-26 20:14:31 -0300

    optimización

---
+++ js/scroll.js
@@ -0,0 +1,37 @@
+function init() {
+    const slider = document.querySelector('.carrusel');
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener('mousedown', (e) => {
+        isDown = true;
+        slider.classList.add('active');
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener('mouseleave', () => {
+        isDown = false;
+        slider.classList.remove('active');
+    });
+    slider.addEventListener('mouseup', () => {
+        isDown = false;
+        slider.classList.remove('active');
+    });
+    slider.addEventListener('mousemove', (e) => {
+        if(!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 2; //scroll-fast
+        slider.scrollLeft = scrollLeft - walk;
+    });
+}
+
+function activarGrabbing() {
+    if (window.matchMedia('(max-width: 992px)').matches) {
+        init();
+    }
+}
+
+document.addEventListener('DOMContentLoaded',activarGrabbing);
+window.addEventListener('resize',activarGrabbing);
