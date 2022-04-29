60d7dafbdf4b44e6470269d362df75fc6babf83f
Matias Bubello <matt.bubello@gmail.com>
2022-04-29 19:53:33 -0300

    header responsive

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,12 @@
-window.addEventListener('scroll', () => {
-   const menu = document.querySelector('header');
-   if (window.scrollY > 10) {
-      menu.setAttribute('id','scroll');
-   } else {
-      menu.setAttribute('id','no-scroll');
-   }
-});
+const menu = document.querySelector('header');
+if (window.matchMedia('(min-width: 992px)').matches) {
+   window.addEventListener('scroll', () => {
+      if (window.scrollY > 10) {
+         menu.setAttribute('id', 'scroll');
+      } else {
+         menu.setAttribute('id', 'no-scroll');
+      }
+   });
+} else {
+   menu.setAttribute('id', 'scroll');
+}
