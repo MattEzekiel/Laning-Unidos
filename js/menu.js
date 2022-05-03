10e27d18630e74b3106e74823a962b508af5dbe0
Matias Bubello <matt.bubello@gmail.com>
2022-05-03 19:08:56 -0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,17 @@
 const menu = document.querySelector('header');
-if (window.matchMedia('(min-width: 992px)').matches) {
-   window.addEventListener('scroll', () => {
-      if (window.scrollY > 10) {
-         menu.setAttribute('id', 'scroll');
-      } else {
-         menu.setAttribute('id', 'no-scroll');
-      }
-   });
-} else {
-   menu.setAttribute('id', 'scroll');
+document.addEventListener('DOMContentLoaded',scroll);
+window.addEventListener('resize',scroll);
+
+function scroll() {
+   if (window.matchMedia('(min-width: 992px)').matches) {
+      window.addEventListener('scroll', () => {
+         if (window.scrollY > 10) {
+            menu.setAttribute('id', 'scroll');
+         } else {
+            menu.setAttribute('id', 'no-scroll');
+         }
+      });
+   } else {
+      menu.setAttribute('id', 'scroll');
+   }
 }
